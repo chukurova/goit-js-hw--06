7ae33b7593d39488550e25afcbb67445c05ab3a8
chukurova <asel.chukurova@gmail.com>
2023-02-20 17:56:06 +0600

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const btn = document.querySelector('.change-color');
+const text= document.querySelector('.color');
+btn.addEventListener('click', changeBgColor);
+
+function changeBgColor(){
+  body.style.backgroundColor = getRandomHexColor();
+  text.textContent = body.style.backgroundColor;
+}
