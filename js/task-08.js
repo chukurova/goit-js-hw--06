25d0ccb56882b2ae4272c1822a8628030e333c12
chukurova <asel.chukurova@gmail.com>
2023-02-20 22:26:55 +0600

    modified

--- js/task-08.js
+++ js/task-08.js
@@ -9,16 +9,9 @@ function onSubmit(action) {
     const formElements = action.currentTarget.elements;
     const email = formElements.email.value.trim();
     const password = formElements.password.value.trim();
-<<<<<<< HEAD
     const formFields = {email, password};
     if(email === '' || password === ''){
        return alert(warning);
-=======
-    const formFields = { email, password };
-    if (email === '' || password === '') {
-        alert(warning);
-        return;
->>>>>>> 4bb9bd84106da61448c91734246f13799aa2a811
     }
     console.log(formFields);
     action.currentTarget.reset();
