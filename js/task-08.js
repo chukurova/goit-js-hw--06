9c50a8152c5139f52e1f5b6f30d534a3d802c7d5
chukurova <asel.chukurova@gmail.com>
2023-02-20 17:48:43 +0600

    task-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
 
+const warning = 'Все поля должны быть заполнены!';
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(action) {
+    action.preventDefault();
+    const formElements = action.currentTarget.elements;
+    const email = formElements.email.value.trim();
+    const password = formElements.password.value.trim();
+    const formFields = {email, password};
+    if(email === '' || password === ''){
+        alert(warning);
+    }
+console.log(formFields);
+action.currentTarget.reset();
+}
